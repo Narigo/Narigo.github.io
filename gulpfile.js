19e64bdbe09c0c454d8a88ecf512ca13ca61d782
Joern Bernhardt <jb@campudus.com>
2015-12-08 19:32:05 +0100

    Create blog post index
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var browserSync = require('browser-sync');
+var del = require('del');
 var handlebars = require('gulp-compile-handlebars');
 var ghPages = require('gulp-gh-pages');
 var markdown = require('gulp-markdown');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
-var del = require('del');
 var source = require('vinyl-source-stream');
+var through = require('through2');
 
 gulp.task('build:assets', copyAssets);
 gulp.task('build:blog:posts', compileBlogPosts);
@@ -55,13 +56,29 @@ function createBlogIndex() {
 
 function compileBlogPosts() {
   return gulp.src(['src/blog/posts/**/*.md'])
+    .pipe(through.obj(function (file, enc, callback) {
+      console.log('args:', file.contents.toString());
+      var post = /^.*#+\s*(.*?)[\r\n]+([\s\S]*?)(?:[\r\n]{2,}|$)/m.exec(file.contents.toString());
+      if (post) {
+        console.log(post);
+        posts.push({
+          link : file.dirname + '/' + file.basename + '.html',
+          title : post[1],
+          short : post[2]
+        });
+      } else {
+        console.log(file);
+      }
+      console.log('through through!');
+      this.push(file);
+      callback();
+    }))
     .pipe(markdown())
     .pipe(rename(function (p) {
-      console.log('rename ', p);
+      console.log('in rename!', p);
       var matched = /^(\d{4})-(\d\d)-(\d\d)-(.*)$/.exec(p.basename);
       p.dirname = matched ? matched.slice(1, 4).join('/') : p.dirname;
       p.basename = matched ? matched[4] : p.basename;
-      posts.push({dir : p.dirname, name : p.basename, ext : '.html'});
     }))
     .pipe(gulp.dest(outDir + '/blog'))
     .pipe(browserSync.stream());
