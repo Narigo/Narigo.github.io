ace43e7df11af271eda5d2a10988c7cfa157d20a
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-08-05 13:22:21 +0200

    Checkout routes before eject
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- _src/.vuepress/enhanceApp.js
+++ _src/.vuepress/enhanceApp.js
@@ -1,4 +1,4 @@
-import fs from "fs";
+import Error404 from "./components/ErrorNotFound.vue";
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -7,5 +7,12 @@ export default ({
   siteData // site metadata
 }) => {
   // ...apply enhancements to the app
-  fs.writeSync("test.txt", JSON.stringify(router.routes));
+  // const obj = router.options.routes;
+  // router.matcher.addRoutes({ path: "*", component: ErrorNotFound });
+	console.log({ router });
+	const allRoutes = router.options.routes;
+	const routes = router.options.routes.slice(0, -1);
+	console.log({ allRoutes, routes });
+	return { Vue, options, router, siteData};
+  // fs.writeFileSync("./test.txt", JSON.stringify(obj));
 };
