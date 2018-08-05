f6f2c450aa56829e661d81fbdd1ba48cdbd6fd98
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-08-05 13:54:09 +0200

    Make custom not found page work
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- _src/.vuepress/enhanceApp.js
+++ _src/.vuepress/enhanceApp.js
@@ -1,18 +1,14 @@
-import Error404 from "./components/ErrorNotFound.vue";
-
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  // ...apply enhancements to the app
-  // const obj = router.options.routes;
-  // router.matcher.addRoutes({ path: "*", component: ErrorNotFound });
-	console.log({ router });
-	const allRoutes = router.options.routes;
-	const routes = router.options.routes.slice(0, -1);
-	console.log({ allRoutes, routes });
-	return { Vue, options, router, siteData};
-  // fs.writeFileSync("./test.txt", JSON.stringify(obj));
+  router.beforeEach((to, from, next) => {
+    if (to.matched.length > 0 && to.matched[0].path === "*") {
+      next("/404.html");
+    } else {
+      next();
+    }
+  });
 };
